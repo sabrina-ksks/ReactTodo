fb066d5712a3adf31a1654196aa28c4df55680a6
sabrina <yhssayah.kosu@gmail.com>
2022-03-13 16:09:49 +0900

    Add: タスク登録用のボタンを追加

--- src/components/TodoBody.tsx
+++ src/components/TodoBody.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 
 import Box from "@mui/material/Box";
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 import TaskList from "./TaskList";
 
 const TodoBody: React.FC = () => {
   return (
-    <Box padding="1rem" textAlign="center">
+    <Box padding="2rem" textAlign="center">
       <TaskList />
+      <Fab
+        color="primary"
+        aria-label="add"
+        sx={{
+          position: 'absolute',
+          bottom: '2rem',
+          right: '2rem'
+        }}
+      >
+        <AddIcon />
+      </Fab>
     </Box>
   );
 };
